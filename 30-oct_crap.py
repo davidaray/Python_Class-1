bfb6145024792b7c6f0644c0b1372340857105b4
Austin B. Osmanski <austin.osmanski@ttu.edu>
2019-10-30 11:52:08 -0500

    Update 30-oct_crap.py

--- 30-oct_crap.py
+++ 30-oct_crap.py
@@ -1,7 +1,6 @@
 import os
 import re
 
-
 for FILE in os.listdir():
     if FILE.endswith(".fasta"):
         OUTPUT = open(FILE+".lft",'w')
@@ -9,13 +8,12 @@ for FILE in os.listdir():
             for LINE in FIN:
                 if LINE.startswith('>'):
                     HEADER = re.sub('>','',LINE)
-                    PART1_HEADER = HEADER.split(":")
+                    HEADER2 = re.sub('\n','',HEADER)
+                    PART1_HEADER = HEADER2.split(":")
                     CONTIG = str(PART1_HEADER[0])
                     PART2_HEADER = PART1_HEADER[1]
                     SPLIT_PART2 = PART2_HEADER.split("-")
                     START = int(SPLIT_PART2[0])
                     END = int(SPLIT_PART2[1])
                     LENGTH = END-START
-                    print(CONTIG)
-                    OUTPUT.write(str(START) + '\t' + str(HEADER) + '\t' + str(LENGTH) + '\t' + str(CONTIG) + '\t' + str(END) + '\n')
-
+                    OUTPUT.write(str(START) + '\t' + str(HEADER2) + '\t' + str(LENGTH) + '\t' + str(CONTIG) + '\t' + str(END) + '\n')
